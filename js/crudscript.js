b8e4296ed05a9d0afa0f50663a014ae374b24ee7
piwica <piwica@gmail.com>
2022-02-24 18:56:08 -0300

    Cargar 3 próximos eventos

--- js/crudscript.js
+++ js/crudscript.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
             url: '/php/oauth/get.php',
             success: function (data) {
                 var json = $.parseJSON(data);
-               $("#login").attr("href", json.href);
+                $("#login").attr("href", json.href);
             },
             error: function () {
                 console.log('There was some error performing the AJAX call!');
@@ -16,8 +16,13 @@ $(document).ready(function () {
             url: '/php/cursos_eventos/list.php',
             data: { limit: 3 },
             success: function (data) {
-                console.log('AJAX call was successful!');
-                console.log('Data from the server' + data);
+                var json = $.parseJSON(data);
+                var evento1 = json[0];
+                var evento2 = json[1];
+                var evento3 = json[2];
+                completarCursoEvento(evento1, 1);
+                completarCursoEvento(evento2, 2);
+                completarCursoEvento(evento3, 3);
             },
             error: function () {
                 console.log('There was some error performing the AJAX call!');
@@ -25,3 +30,30 @@ $(document).ready(function () {
         }
     );
 })
+
+function completarCursoEvento(evento, indice) {
+    var tipoEvento = parsearEvento(evento["tipo_evento"]);
+    $("#cursos-eventos-tipo-" + indice).html(tipoEvento);
+    $("#cursos-eventos-imagen-" + indice).attr("src", evento["url_imagen"]);
+    $("#cursos-eventos-titulo-" + indice).html(evento["titulo"]);
+    $("#cursos-eventos-lugar-" + indice).html(evento["lugar"]);
+    $("#cursos-eventos-horario-" + indice).html(evento["fecha"]);
+    $("#cursos-eventos-cursos-eventos-masinfo-" + indice).attr("href", "cursos_y_eventos/id/" + evento["curso_evento_id"]);
+
+    $("#cursos-evento-skeleton-" + indice).addClass("d-none");
+    $("#cursos-evento-card-" + indice).removeClass("d-none");
+}
+
+function parsearEvento(tipoEvento) {
+    var resultado = "Evento";
+    if (tipoEvento === "stream") {
+        resultado = "Live";
+    } else if (tipoEvento === "historia") {
+        resultado = "Conmemoración";
+    } else if (tipoEvento === "curso") {
+        resultado = "Curso";
+    } else if (tipoEvento === "prostesta") {
+        resultado = "Protesta";
+    }
+    return resultado;
+}
